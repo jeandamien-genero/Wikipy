e117354f04b9da812f27ad6f57cebf7d1889948f
jeandamien-genero <jeandamien.genero@gmail.com>
2020-05-24 19:25:16 +0200

    using a function

--- replace_legifrance.py
+++ replace_legifrance.py
@@ -20,15 +20,17 @@
         </ref>
 
     author : Jean-Damien Généro
-    date : 22 mai 2020
+    date : 23 mai 2020
 """
 
+# 1. Imports
 import re
 import requests
 from bs4 import BeautifulSoup
 import pywikibot
 
 
+# 2. Functions
 def replace_all(article, dic):
     """"Replaces in a text a dict key by its value.
     :param article: a wp page
@@ -41,44 +43,76 @@ def replace_all(article, dic):
         article = article.replace(key, value)
     return article
 
-# 1. Settings
+
+def fill_lgf_template(article, patterns_dict):
+    """Replaces any refs that links to the website of official french laws and decrees
+       (www.legifrance.gouv.fr.) by the structured template {{Légifrance}} (french wikipedia).
+    :param article: a wikipedia parge parsed by pywikibot.
+    :type article: pywikibot.page.Page
+    :param patterns_dict: a dictionary containing patterns of the old refs as its keys (str)
+                          and the position of the url's captured group as its values (int).
+    :type patterns_dict: dict
+    :return: a dict with the old refs as keys and the new as values.
+    :rtype: dict
+    """
+    dict_ref = {}
+    for pattern in patterns_dict:
+        search = re.findall(pattern, article.text)
+        for item in search:
+            r_search = requests.get(
+                item[patterns_dict[pattern]])
+            soup = BeautifulSoup(r_search.text, 'html.parser')
+            title = soup.html.head.title.text
+            title_pattern = '((Décret)?(Arrêté)? du \d? \w+ \d{4} [\w éèêôîûùïë\',\(\)\-\.]+)'
+            title_search = re.search(title_pattern, title)
+            nor_pattern = 'I?P?D?A?F?[A-Z]{3}[0-9]{7}[A-Z]'
+            nor = re.findall(nor_pattern, r_search.text)
+            if nor:
+                model = '<ref{}>{{{{Légifrance|base=JORF|numéro={}|texte={}}}}}.</ref>'.format(
+                    item[1], nor[0], title_search.group(1))
+                dict_ref[item[0]] = model
+    return dict_ref
+
+
+# 3. Settings
+
+dict_search = {
+    '(<ref( name=\\"promo_[a-z]+\\")?>{{Article \|titre= ?((Décret)?(Arrêté)? du [1-9]\d? [a-zé]+ '
+    '\d{4}[a-z \'éêè,{}0-9A-Zà\(\)-]+) ? ?\|périodique=Journal officiel de la République française'
+    ' ?\|(lien périodique=Journal officiel de la République française ?\|)?numéro=\d+ \|jour=[1-9]'
+    '\d? ?\|mois=[a-zé]+ ?\|année=\d{4} ?\|lire en ligne=(http(s)?:\/\/www\.legifrance\.gouv\.fr\/'
+    'affichTexte\.do\?cidTexte=JORFTEXT\d+)([&categorieLien=id]+)?( )?}}\.<\/ref>)' : 6,
+
+    '(<ref( name=\\"[a-zéA-Z_0-9 ]+\\")?>{{lien web\|titre=((Décret)?(Arrêté)? du ({{)?[1-9]\d?(er)?'
+    '(}})? [a-zé]+ \d{4}[a-z \'éèê,{}0-9A-Zà\(\)-\|]+) ?\|site=(http(s)?:\/\/www\.)?legifrance.gouv.'
+    'fr\/? ?\|périodique=\[?\[Journal.+\]\]?(, {{numé\w+\|\d+}})? ?\|((issn=\d+-\d+\|())?)(date=[1-9]'
+    '\d? [a-zé]+ [0-9]{4}\|())?(issn=\d+-\d+\|()?)(date=[1-9]\d? [a-zé]+ [0-9]{4}\|())?(lire en ligne'
+    ')?(url)?=(https:\/\/www\.legifrance\.gouv\.fr\/affichTexte\.do\?cidTexte=JORFTEXT[0-9]+)(&date'
+    'Texte=)?(&categorieLien=id)? ?}}\.?<\/ref>)' : 22,
+
+    '(<ref( name=\\"[a-zéA-Z_0-9 ]+\\")?>{{lien web\|url=(http(s)?:\/\/www\.legifrance\.gouv\.fr\/'
+    'affichTexte\.do\?cidTexte=JORFTEXT\d+)(&dateTexte=)?(&categorieLien=id)?\|titre=((Décret)?'
+    '(Arrêté)? du ({{)?[1-9]\d?(er)?(}})? [a-zé]+ \d{4}[a-z \'éèê,{}0-9A-Zà\(\)-\|]+) ?\|date=[1-9]'
+    '\d? [a-zé]+ \d{4}\|site=(http(s)?:\/\/www\.)?legifrance\.gouv\.fr\/? ?\|(périodique)?(éditeur)?'
+    '=\[?\[Journal.+\]\]?(, {{numé\w+\|\d+}})? ?\|(issn=\d+-\d+\|)?consulté le=\d{2} [a-z]+ \d{4}}}'
+    '\.<\/ref>)' : 2,
+
+    '(<ref( name=\\"[a-zéûA-Z_0-9 ]+\\")?>\[(http(s)?:\/\/www\.legifrance\.gouv\.fr\/affichTexte\.do'
+    '([\w\-&%=\?\.;,! ]+)?)\s[A-Z].+\]\.?.+?\.?<\/ref>)' : 2,
+
+    '(<ref( name=\\"[a-zéA-Z_0-9 ]+\\")?>{{\w+ ?(\w+)?\|titre=(Décret)?(Arrêté)? du ({{)?[1-9]\d?(er)?'
+    '(}})? [a-zé]+ \d{4}[a-z \'éè,{}0-9A-Zà\(\)-\|]+ ?\|lire en ligne=(((http(s)?:\/\/www\.legifrance'
+    '\.gouv\.fr\/affichTexte\.do\?cidTexte=JORFTEXT\d+)(&dateTexte=)?(&categorieLien=id)?)) ?\|consulté'
+    'le=(\d{2} [a-z]+ \d{4})?(\d{4}-\d{2}-\d{2})?(\d{2}-\d{2}-\d{4})?}}<\/ref>)' : 10,
+
+    '(<ref( name=\\"promo_[a-z]+\\")?>\[?(http(s)?:\/\/www\.legifrance\.gouv\.fr\/affichTexte\.do([\w\-&'
+    '%=\?\.;,! ]+)?)\]?<\/ref>)' : 2
+}
+
 site = pywikibot.Site()
 page = pywikibot.Page(site, u"__page that will be modify___")
-dict_ref = {}
 
-# 2. Pattern
-legifrance = '(<ref( name=\\"[a-z_]+\\")?>{{Article \\|titre= ?((Décret)?(Arrêté)? du [1-9]\
-              [0-9]? [a-zé]+ [0-9]{4}[a-z \'é,{}0-9A-Zà\\(\\)-]+) ?\\|périodique=Journal officiel\
-               de la République française ?\\|(lien périodique=Journal officiel de la République \
-               française ?\\|)?numéro=[0-9]+ \\|jour=[1-9][0-9]? ?\\|mois=[a-zé]+ ?\\|année=[0-9]\
-               {4} ?\\|lire en ligne=(https:\\/\\/www\\.legifrance\\.gouv\\.fr\\/affichTexte\\.do\
-               \\?cidTexte=JORFTEXT[0-9]+)([&categorieLien=id]+)?( )?}}\\.<\\/ref>)'
-"""
-                                    ======= Captured groups =======
-                | 0. all | 1. @name in <ref> | 2. decree's title | 3. str 'Décret' |
-                | 4. str 'Arrêté' | 5. blank | 6. decree url | 7. blank | 8. blank |
-"""
 
-# 3. Searching for all regex occurrences in the wp page.
-search = re.findall(legifrance, page.text)
-
-# 4. Iterating on each result of the previous search
-for item in search:
-    # Making a request for the decree :
-    r_legifrance = requests.get(item[6])
-    # Making a soup with it :
-    soup = BeautifulSoup(r_legifrance.text, 'html.parser')
-    # Defining a pattern for the NOR number and searching for all its occurrences in the decree :
-    nor_pattern = 'P?D?A?[A-Z]{3}[0-9]{7}[A-Z]'
-    NOR = re.findall(nor_pattern, r_legifrance.text)
-    # When a result is found, we make the legifrance template using it and the 1st regex's results :
-    if NOR:
-        model = '<ref{}>{{{{Légifrance|base=JORF|numéro={}|texte={}}}}}.</ref>'\
-                .format(item[1], NOR[0], item[2])
-        # adding to the dict the old journal template and the new legifrance template
-        dict_ref[item[0]] = model
-
-# 5. Writing the changes in the wp page by applying the replace_all function :
-page.text = replace_all(page.text, dict_ref)
-print(page.text)
-page.save(u"bot : __modification message__")
+# 4. Writing the changes in the wp page by applying the fill_lgf_template function :
+page.text = replace_all(page.text, fill_lgf_template(page, dict_search))
+page.save(u"bot : intégration du modèle Légifrance dans les références pointant vers le JORF")
