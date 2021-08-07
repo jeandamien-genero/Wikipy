a65583378e9dd26d2733b90ba22452605a7b798f
Jean-Damien Généro <57401299+jeandamien-genero@users.noreply.github.com>
2021-08-07 11:50:22 +0200

    update : two functions

--- updating_fr_ambassadors.py
+++ updating_fr_ambassadors.py
@@ -1,150 +1,125 @@
 #!/usr/bin/python
 # -*- coding: UTF-8 -*-
 
+
 """
-    author : Jean-Damien Généro
-    date : 10 octobre 2020
-    Script that updates wp:fr's list of current french ambassadors ("Liste des ambassadeurs français", (https://fr.wikipedia.org/wiki/Liste_des_ambassadeurs_fran%C3%A7ais) with data from a JSON of the JORFSearch website (https://jorfsearch.steinertriples.fr/tag/ambassadeur_pays?format=JSON).
+author : Jean-Damien Généro
+Date : 2020-10-08
+Update : 2021-08-07
+Updating wp:fr's list of current french ambassadors
+python3 pwb.py ambassadors2021.py
 """
 
-# IMPORTS
+import re
+import pywikibot
+import requests
+import sys
 import datetime
 import locale
-import pywikibot
-import re
-import requests
 
 
-# REQUEST FOR THE JSON
-r = requests.get("https://jorfsearch.steinertriples.fr/tag/ambassadeur_pays?format=JSON")
-j = r.json()
-
-# PYWIKIBOT CONFIGURATION
-site = pywikibot.Site()
-page = pywikibot.Page(site, "Liste des ambassadeurs français")
-
-# SETTING LOCALE TIME
 locale.setlocale(locale.LC_TIME, 'fr_FR.UTF-8')
 
-# SETTING EMPTY DICTS AND LISTS
-ambassades = {}
-diplodict = {}
-wrong = {}
-good = []
 
-# SETTING DICTS AND LIST
-# JSON forms : WP forms
-corrects_forms = {"Arabie Saoudite": "Arabie saoudite", "République algérienne démocratique populaire": "Algérie",
-                  "République démocratique fédérale d'Ethiopie": "Éthiopie", "Îles Salomon": "Salomon",
-                  "Vietnam": "Viêt Nam", "Etats-Unis": "États-Unis", "Somalie": "Somalie",
-                  "Afrique du sud": "Afrique du Sud", "Jamaique": "Jamaïque"}
-
-# special ref for non-resident ambassadors
-non_resident = {"Afrique du Sud": "<ref group=n>Également ambassadeur auprès du [[Lesotho]].</ref>",
-                "Fidji": "<ref group=n>Également ambassadeur auprès de [[Nauru]], des [[Tuvalu]], des [[Kiribati]] et "
-                         "des [[Tonga]].</ref>",
-                "Gabon": "<ref group=n>Également ambassadeur auprès de [[Sao Tomé-et-Principe]].</ref>",
-                "Guinée": "<ref group=n>Également ambassadeur auprès du [[Sierra Leone]].</ref>",
-                "Indonésie": "<ref group=n>Également ambassadeur auprès du [[Timor oriental]].</ref>",
-                "Italie": "<ref group=n>Également ambassadeur auprès de [[Saint-Marin]].</ref>",
-                "Mozambique": "<ref group=n>Également ambassadeur auprès du [[Swaziland]].</ref>",
-                "Nouvelle-Zélande": "<ref group=n>Également ambassadeur auprès des [[îles Cook]] et des [[îles "
-                                    "Samoa]].</ref>",
-                "Panama": "<ref group=n>Également ambassadeurs auprès des [[Bahamas]].</ref>",
-                "Papouasie-Nouvelle-Guinée": "<ref group=n>Également ambassadeur auprès des [[îles Salomon]].</ref>",
-                "Philippines": "<ref group=n>Également ambassadeur auprès des [[îles Marshall]], de la [[États "
-                               "fédérés de Micronésie|Micronésie]] et des [[Palaos]].</ref>",
-                "Sainte-Lucie": "<ref group=n>Également ambassadeur auprès d'[[Antigua-et-Barbuda]], de la [["
-                                "Barbade]], de la [[Dominique (pays)|Dominique]], de la [[Grenade (pays)|Grenade]], "
-                                "de [[Saint-Christophe-et-Niévès]] et de [[Saint-Vincent-et-les-Grenadines]].</ref>",
-                "Salvador": "<ref group=n>Également ambassadeur auprès du [[Belize]].</ref>",
-                "Sénégal": "<ref group=n>Également ambassadeur auprès de la [[Gambie]].</ref>",
-                "Sri Lanka": "<ref group=n>Également ambassadeur auprès des [[Maldives]].</ref>",
-                "Suisse": "<ref group=n>Également ambassadeur auprès du [[Liechtenstein]].</ref>",
-                "Suriname": "<ref group=n>Également ambassadeur auprès du [[Guyana]].</ref>",
-                "Trinité-et-Tobago": "<ref group=n>Également ambassadeur auprès de la [[Barbade]].</ref>",
-                "Zimbabwe": "<ref group=n>Également ambassadeur auprès du [[Malawi]].</ref>"}
-
-# lines that will not be changed (2nd & co lines : non-resident ambassadors)
-no_modif = ["Russie", "Laos", "États-Unis", "Cameroun",
-            "Nauru", "Tuvalu", "Kiribati", "Tonga", "Sao Tomé-et-Principe", "Sierra Leone", "Timor oriental",
-            "Saint-Marin", "Swaziland", "Îles Cook", "Samoa", "Îles Samoa", "Bahamas", "Salomon", "Îles Marshall",
-            "Micronésie", "Palaos", "Antigua-et-Barbuda", "Barbade", "Dominique", "Grenade",
-            "Saint-Christophe-et-Niévès", "Saint-Vincent-et-les-Grenadines", "Belize", "Gambie", "Maldives",
-            "Liechtenstein", "Guyana", "Barbade", "Malawi", "Eswatini", "Lesotho", "Saint-Vincent-et-les Grenadines"]
-
-# GETTING INFOS FROM THE JSON
-for item in j:
-    pays = item["ambassadeur_pays"]
-    if pays not in ambassades.keys():
-        ambassades[pays] = ["{} {}".format(item["prenom"], item["nom"]),
-                            item["source_date"],
-                            item["source_id"]]
-# print(ambassades)
-
-# MAKING A DICT where "Country" = ["ambassador", "date", "ref JORFTEXT\d+"]
-for key, value in ambassades.items():
-    year = re.sub(r'(\d{4})-\d{2}-\d{2}', '\\1', value[1])
-    if int(year) > 2013:
-        # print("{} : {}".format(key, value))
-        if key in corrects_forms.keys():
-           key = corrects_forms[key]
-           diplodict[key] = value
-        else:
-            diplodict[key] = value
-# print(diplodict)
+# json_url = sys.argv[1]
 
 
-"""
-# CHECKING IF ALL THE COUNTRIES ARE IN THE WP PAGE
-for country in diplodict:
-    if country in page.text:
-        print("------> {}".format(country))
-    else:
-        print("--//--> {}".format(country))
-        # countries with // need to be added to corrects_forms."""
+def ambassadors(json_url):
+    """
+    parsing a JSON with all ambassadors nominations and making a list with only the last ones
+    :param json_url: URL to a JSON with nomination
+           (https://jorfsearch.steinertriples.fr/tag/ambassadeur_pays?format=JSON)
+    :type json_url: str
+    :return: a list with all last nominations by country
+           ['ambassador', 'JORF date', 'country', 'JORF text id', 'Jorf date - one day', 'nomination year']
+           >> ['Elisabeth Barsacq', '2020-10-02', 'Albanie', 'JORFTEXT000042387663', '01-octobre-2020', '2020']
+    :rtype: list
+    """
+    r = requests.get(json_url)
+    j = r.json()
+    # 1/ MAKING LISTS WITH ambassadors + countries
+    ambassadeurs = []
+    all_countries = []
+    # dict with JORF forms: WP forms
+    forms = {"République dominicaine": "Dominique", "Arabie saoudite": "Arabie Saoudite", "États-Unis": "Etats-Unis",
+             "Fidji": "Fidji", "Guatemala": "République du Guatémala", "Israël": "Israël", "Japon": "Japon",
+             "Jordanie": "Jordanie", "Laos": "Laos", "République de Macédoine": "Macédoine", "Rwanda": "Rwanda",
+             "Turquie": "Turquie", "Viêt Nam": "Vietnam"}
+    for item in j:
+        if item["type_ordre"] == "nomination":
+            individual = item["prenom"] + " " + item["nom"]
+            d = datetime.datetime.strptime(item["source_date"], '%Y-%m-%d')
+            date = '{:%Y-%m-%d}'.format(d)
+            country = item["ambassadeur_pays"]
+            for wrong in forms:
+                if country == forms[wrong]:
+                    country = country.replace(forms[wrong], wrong)
+            all_countries.append(country)
+            txt = item["source_id"]
+            ambassadeurs.append([individual, date, country, txt])
+    countries = set(all_countries)
+    # 2/ MAKING A DICT WITH "country": [ambassadors]
+    insittutions = {}
+    for country in countries:
+        insittutions[country] = []
+    for item in ambassadeurs:
+        insittutions[item[2]].append(item)
+    # 3/ SEARCHING LAST NOMINATION
+    last_nomintions = []
+    for country_name in insittutions:
+        time = []
+        for individual_infos in insittutions[country_name]:
+            # making a list with date only in order to pick the most recent one
+            time.append(individual_infos[1])
+        jo_date = datetime.datetime.strptime(max(time), '%Y-%m-%d')
+        for individual_infos in insittutions[country_name]:
+            if '{:%Y-%m-%d}'.format(jo_date) in individual_infos[1]:
+                # decree date = jorf date - 1 day
+                old_decree = jo_date - datetime.timedelta(days=1)
+                decree = '{:%d-%B-%Y}'.format(old_decree)
+                year = '{:%Y}'.format(old_decree)
+                individual_infos.append(decree)
+                individual_infos.append(year)
+                last_nomintions.append(individual_infos)
+    return last_nomintions
 
 
-# MAKING A LIST OF COUNTRY FOR WHOM CHANGE IS NEEDED
-for country in diplodict:
-    url = "https://www.legifrance.gouv.fr/jorf/id/{}".format(diplodict[country][2])
-    decree = requests.get(url)
-    try:
-        title = re.findall(r'(Décret du \d{1,2} [\wéèû]+ \d{4})', decree.text)
-        ref = "<ref>{{{{Légifrance|base=JORF|url={} |texte={}}}}}.</ref>".format(url, title[0])
-    except IndexError:
-        title = "Décret de nomination de " + diplodict[country][0]
-        ref = "<ref>{{{{Légifrance|base=JORF|url={} |texte={}}}}}.</ref>".format(url, title)
-        title_date = "UNFOUND fo {}".format(country)
-        continue
-    title = re.findall(r'(Décret du (\d{1,2} [\wéèû]+ \d{4}))', decree.text)
-    # print(title[0])  # ('Décret du 10 septembre 2020', '10 septembre 2020')
-    ref = "<ref>{{{{Légifrance|base=JORF|url={} |texte={}}}}}.</ref>".format(url, title[0][0])
-    title_date = datetime.datetime.strptime(str(title[1][1]), '%d %B %Y')
-    decreedate = title_date.strftime('%d|%B|%Y').lstrip("0").replace(" 0", " ")
-    # print(decreedate)  # 10|septembre|2020
-    nomination_date4 = "{{{{tri date|{}}}}}".format(decreedate)
-    # print(nomination_date4)  # {{tri date|10|septembre|2020}}
-    if nomination_date4 not in page.text and country not in no_modif:
-        # print("--//--> {} ({} : {})".format(country, title[0], url))
-        wrong[country] = [nomination_date4, ref]
+def updating_ambassadors(article, unwanted_changes) -> None:
+    site = pywikibot.Site()
+    page = pywikibot.Page(site, article)
+    MAJ = []
+    for nomination in ambassadors("https://jorfsearch.steinertriples.fr/tag/ambassadeur_pays?format=JSON"):
+        # nomination[2] = country
+        if nomination[2] not in unwanted_changes:
+            # nomination[5] = nomination year
+            if nomination[5] == '2020' or nomination[5] == "2021":
+                # nomination[3] = JORF text id
+                if nomination[3] not in page.text:
+                    # search = country (nomination[2])'s line in page.text
+                    search = re.findall('(\| +\{\{' + nomination[2] + '\}\} +\|\|.+\|\|.+\|\| align=\"right\" \| ?{{[tT]ri date\|\d+\|[éèûa-zA-Z0-9]+\|\d+)', page.text)
+                    try:
+                        search[0]
+                    except IndexError:
+                        continue
+                    new_year = search[0] + nomination[5]  # country'line + new year
+                    if new_year not in page.text:
+                        new_year = re.sub(r"[tT]ri +date.+", "tri date|" + nomination[4].replace('-', '|'), new_year)
+                        new_year = new_year + "}} || <ref>{{Légifrance|base=JORF|url=https://www.legifrance.gouv.fr/jorf/id/"+ nomination[3] + "|texte=Décret du " + nomination[4].replace('-', ' ') + "}}.</ref>"
+                        page.text = page.text.replace(search[0], new_year)
+                        page.text = re.sub(r'(\| +\{\{' + nomination[2] + '\}\} +\|\|.+\|\|).+(\|\| align=\"right\")',
+                                           "\\1[[" + nomination[0] + "]]\\2", page.text)
+                        page.text = re.sub(r"</ref>}}.+(\n</ref>)?", "</ref>", page.text)
+                        # adding country name to the MAJ list for the commit message
+                        MAJ.append(nomination[2])
+    # updating the update date at the top of the page
+    today = datetime.date.today()
+    thisday = today.strftime("%d %B %Y")
+    page.text = re.sub(r"La liste qui suit a été mise à jour le \d+ [\wéèû]+ \d+", "La liste qui suit a été mise à jour le " + thisday, page.text)
+    # deal with exep here if needed
+    # print(page.text)
+    page.save("mise à jour automatique : {}".format(", ".join(sorted(MAJ))))
+    print("DONE")
 
-print(wrong)
-# CHANGING WP PAGE TEXT
-for item in wrong:
-    new_ambassador = diplodict[item][0]
-    if item in non_resident:
-        pattern0 = r'(\| {{' + item + '}} ?\|{2} ?\[{2}Am.+\]{2} ?\|{2} ?)[\[{]?[\[{]?.+[\]}]?[\]}]?((<ref group=n>.+<\/ref>)? ?\|{2} ?align="right" ?\| ?){{t?T?ri ?date\|\d{1,2}\|.+\|\d{4}}}( {0,}\|{1,2} {0,})(<ref>.+\n?<\/ref>)?({{,}}<ref>.+\n?<\/ref>)?'
-        page.text = re.sub(pattern0,
-                           "\\1[[{}]]{}\\2{}\\3 || {}".format(diplodict[item][0], non_resident[item], wrong[item][0], wrong[item][1]),
-                           page.text)
-    else:
-        pattern0 = r'(\| {{' + item + '}} ?\|{2} ?\[{2}Am.+\]{2} ?\|{2} ?)[\[{]?[\[{]?.+[\]}]?[\]}]?((<ref group=n>.+<\/ref>)? ?\|{2} ?align="right" ?\| ?){{t?T?ri ?date\|\d{1,2}\|.+\|\d{4}}}( {0,}\|{1,2} {0,})(<ref>.+\n?<\/ref>)?({{,}}<ref>.+\n?<\/ref>)?'
-        page.text = re.sub(pattern0,
-                           "\\1[[{}]]\\2{}\\3 || {}".format(diplodict[item][0], wrong[item][0], wrong[item][1]),
-                           page.text)
-# print(page.text)
 
-# SAVING PAGE
-page.save("mise à jour : " + ', '.join(wrong))
-print("mise à jour : " + ', '.join(wrong))
+no_need = ["Arabie saoudite", "Australie", "Biélorussie", "Bosnie-Herzégovine", "Brésil", "Colombie", "Finlande",
+           "Kazakhstan", "Lettonie", "Paraguay", "Russie"]
